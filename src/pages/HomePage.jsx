f3274b109f5f37a47e772b2019a5d0d9d3dcc9f3
Aitor Ramoneda <ramoneda.aitor@gmail.com>
2024-10-10 11:35:56 +0200

    Retoques de la home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
-import { Typography } from '@mui/material';
+import { Chip, Typography } from '@mui/material';
 
 export const HomePage = () => {
     
@@ -9,7 +9,8 @@ export const HomePage = () => {
     return (
         <>
             <Typography variant="h1" sx={{ fontSize: { xs: "24px", sm: "32px", md: "38px" }, mb: 2 }}>Hola { user.nombre }!</Typography>
-            <Typography>Bienvenido a la página principal. Desde esta aplicación podrás comprobar los datos de tu targeta de transporte.</Typography>
+            <Typography>Bienvenido a la página principal. Desde esta aplicación podrás comprobar los datos de tu targeta de transporte de banda magnética.</Typography>
+            <Chip label='Dirígete al apartado "Visualización" para leer los datos de la targeta' color="primary" variant="outlined" sx={{ mt: 2, mb: 2 }} />
         </>
     )
 }
